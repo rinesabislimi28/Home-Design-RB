9e7aff24ef7654a982f44b72f9cad0a1e6cf96b9
rinesabislimi28 <rinesabislimi28@gmail.com>
2023-12-11 23:51:21 +0100

    krijimi i login, ndryshime ne faqet e tjera

---
+++ js/login.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+        const pwIcon = document.querySelector('.rb-eyes-icon');
+        const pwField = document.querySelector('.rb-password');
+
+        pwIcon.addEventListener('click', function () {
+            if (pwField.type === 'password') {
+                pwField.type = 'text';
+                pwIcon.classList.replace('fa-eyes-slash', 'fa-eye');
+            }
+            else {
+                pwField.type = 'password';
+                pwIcon.classList.replace('fa-eye', 'fa-eye-slash');
+            }
+        });
+    });

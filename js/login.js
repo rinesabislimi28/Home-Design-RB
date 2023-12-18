964aaefd27f26c9dc915f958bd9569f1fa3460e3
rinesabislimi28 <rinesabislimi28@gmail.com>
2023-12-18 13:02:39 +0100

    test-commit

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function() {
         const pwIcon = document.querySelector('.rb-eyes-icon');
         const pwField = document.querySelector('.rb-password');
 

0fb7efd15ec5dbd9c20e9cbfd8fbc6cc5dadf6e0
rinesabislimi28 <rinesabislimi28@gmail.com>
2023-12-24 14:57:05 +0100

    1

--- js/video.js
+++ js/video.js
@@ -7,11 +7,17 @@ document.addEventListener('DOMContentLoaded', function(){
     btn.onclick = function() {
       btn.classList.add('rb-active');
       video.classList.add('rb-active');
+      btn.querySelector('.rb-play').classList.add('hide-button')
+      document.querySelector('footer').style = 'display: none'
+      document.querySelector('header').style = 'display: none'
       videos.play();
     }
     close.onclick = function(){
       btn.classList.remove('rb-active');
       video.classList.remove('rb-active');
+      btn.querySelector('.rb-play').classList.remove('hide-button')
+      document.querySelector('footer').style = ''
+      document.querySelector('header').style = ''
       videos.pause();
     }
 });

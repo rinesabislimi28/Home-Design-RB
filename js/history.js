64af46ef7969fce9d255b6dad795ee6c23dfa3e4
rinesabislimi28 <rinesabislimi28@gmail.com>
2024-01-03 22:38:29 +0100

    Create checkbox with html, css and js

--- js/history.js
+++ js/history.js
@@ -1,8 +1,9 @@
-//Section change color with keyboard-event
-document.addEventListener("DOMContentLoaded", function() {
-    let section = document.querySelector(".rb-section-history");
-    document.addEventListener("keydown", function(event) {
-    if(event.key === 'r' || event.key === 'R')
-        section.style.backgroundColor = "red";
-    });
+//Section change color with switch checkbox 
+let checkbox = document.getElementById('rb-checkbox');
+let section = document.querySelector('.rb-section-history');
+
+checkbox.addEventListener('change', function(){
+    section.style.backgroundColor = this.checked ? 'red' : 'green';
 });
+
+

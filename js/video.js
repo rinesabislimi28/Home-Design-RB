46d4eac5913861dd36ffe6ec685e57d32878f722
rinesabislimi28 <rinesabislimi28@gmail.com>
2024-01-07 00:24:44 +0100

    adjusting the navbar , and some changes in all pages

--- js/video.js
+++ js/video.js
@@ -1,4 +1,4 @@
-//Section 
+//Section for video
 document.addEventListener('DOMContentLoaded', function(){
     let btn = document.querySelector('.rb-btn');
     let video = document.querySelector('.rb-video');
@@ -7,17 +7,17 @@ document.addEventListener('DOMContentLoaded', function(){
     btn.onclick = function() {
       btn.classList.add('rb-active');
       video.classList.add('rb-active');
-      btn.querySelector('.rb-play').classList.add('hide-button')
-      document.querySelector('footer').style = 'display: none'
-      document.querySelector('header').style = 'display: none'
+      btn.querySelector('.rb-play').classList.add('hide-button');
+      document.querySelector('footer').style = 'display: none';
+      document.querySelector('header').style = 'display: none';
       videos.play();
     }
     close.onclick = function(){
       btn.classList.remove('rb-active');
       video.classList.remove('rb-active');
-      btn.querySelector('.rb-play').classList.remove('hide-button')
-      document.querySelector('footer').style = ''
-      document.querySelector('header').style = ''
+      btn.querySelector('.rb-play').classList.remove('hide-button');
+      document.querySelector('footer').style = '';
+      document.querySelector('header').style = '';
       videos.pause();
     }
 });
